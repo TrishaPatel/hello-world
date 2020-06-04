ad33f83d607d67c1899c7d1e71e7be5cf9b2b4b1
Trisha Patel <trisha@curbie.ca>
2020-06-03 18:47:26 -0600

    Added code to display entered name with welcome

---
+++ src/components/UserWelcome.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input v-model="name" placeholder="Enter your Name" />
+    <p v-if="name">Welcome {{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UserWelcome",
+  props: {
+    name: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+p {
+  color: #42b983;
+}
+</style>
